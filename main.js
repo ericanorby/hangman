7757ff9d316964350e8ba54f5c46ae414ccca56a
Erica Norby <ericanorby@gmail.com>
2017-01-29 01:08:24 -0500

    added gray out function

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //word or phrase to be solved
-var phrase = "i love javascript";
+var phrase = "I LOVE JAVASCRIPT";
 var letters;
 
 //splits phrase into an array
@@ -14,7 +14,8 @@ function createLetters(){
   for (var i = 0; i < letters.length; i++){
     var blank = $("<div></div>")
     if (letters[i] != ' ') {
-      blank.addClass("blank-letter");
+      blank.addClass("blank-letter")
+      blank.html(letters[i])
       $(word).append(blank)
     } else {
       var space = $("<div></div>")
@@ -37,10 +38,28 @@ function createAlphabet(){
     var link = $("<a></a>").addClass("letter")
     $(link).attr("href","#")
     $(".alphabet").append(link)
-    $(link).html(letter)
+    $(link).text(letter)
   }
 }
-
 createPuzzle(phrase);
 createLetters();
 createAlphabet();
+
+//when a letter of the alphabet is clicked:
+$("a").click(grayedOut)
+  //make the letter in the alphabet grayed out
+function grayedOut(){
+  $(this).css("color", "lightgray")
+  for (var i = 0; i < letters.length; i++) {
+    if ($(this).text() == letters[i]) {
+      test();
+    }
+  }
+}
+  //if the letter clicked is included in the phrase:
+    //make the letter in the phrase appear
+  //if the letter clicked is NOT included in the phrase:
+    //add next piece of the skeleton
+function test(){
+  $("body").css("background","green")
+}
