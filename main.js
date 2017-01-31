45835337ca488b21e78bd8f79aad82c47d9708af
Erica Norby <ericanorby@gmail.com>
2017-01-30 22:31:47 -0500

    added categories object, randomize phrase, categories panel

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 // $(document).ready(function(){
+var categories = {
+  songs: ["twinkle twinkle little star","what a wonderful world","smells like teen spirit","all you need is love","rolling in the deep","hotel california","cry me a river","stairway to heaven","wonderwall","somewhere over the rainbow"],
+  food: ["chicken pot pie","cheeseburger","cheese pizza","meatball sub","fettucine alfredo","taco salad","potato casserole","omelette","chicken noodle soup","coconut shrimp"],
+  colors: ["chartreuse","camouflage green","burnt sienna","lavender","cerulean","crimson","sky blue","fuchsia","magenta","turquoise"],
+  countries: ["guatemala","indonesia","australia","mozambique","sierra leone","switzerland","zimbabwe","ireland","germany","south korea"]
+}
+
+var random = Math.floor(Math.random() * 10);
+var phrase = categories.songs[random];
+
 
 //word or phrase to be solved
-var phrase = "kitties are awesome";
+
 // var phrase = prompt("enter a word or phrase");
 var letters;
 
 //splits phrase into an array
-function createPuzzle(phrase){
+function createPuzzle(){
 
   letters = phrase.toUpperCase().split('');
+  console.log(letters);
   letters.push(' ');
 }
 
@@ -17,15 +28,13 @@ function createLetters(){
   var word = $("<div></div>").addClass("word")
   $("#letter-board").append(word)
   for (var i = 0; i < letters.length; i++){
-    var blank = $("<div></div>")
+    var blank = $("<div></div>").addClass("blank-letter")
     if (letters[i] != ' ') {
-      blank.addClass("blank-letter")
       blank.html(letters[i])
       $(word).append(blank)
     } else {
       var space = $("<div></div>")
       $("#letter-board").append(space)
-      blank.addClass("blank-letter")
       blank.css("border","none")
       $(space).append(blank)
       var word = $("<div></div>").addClass("word")
@@ -54,18 +63,24 @@ var lettersCompleted = 0;
 function guessedLetter(){
   event.preventDefault();
   //make the letter in the alphabet grayed out
-  $(this).css("color", "rgb(239, 243, 249)")
+  $(this).css("color", "lightgray")
+  $(this).removeAttr("href")
+  $(this).css("pointer-events","none")
   //if the letter clicked is included in the phrase:
     //make the letter in the phrase appear
-  if (letters.indexOf($(this).text()) != -1) {
-    for (var i = 0; i < letters.length; i++) {
+  if (letters.indexOf($(this).text()) !== -1) {
+    for (var i = 0; i <= letters.length; i++) {
       var chosenLetter = letters.indexOf($(this).text(), i)
       $(".blank-letter").eq(chosenLetter).css("color","black")
+      // console.log('chosen Letter ---' + chosenLetter)
+      // console.log($(".blank-letter").eq(chosenLetter));
+      // console.log('these letters---' + letters[i])
       if (letters[i] == $(this).text()) {
         lettersCompleted++
       }
     }
     setTimeout(winGame, 800);
+
   }
   //if the letter clicked is NOT included in the phrase:
     //add next piece of the skeleton
@@ -74,6 +89,7 @@ function guessedLetter(){
     decreaseScore();
     setTimeout(loseGame, 800);
   }
+
 }
 
 createPuzzle(phrase);
@@ -95,6 +111,7 @@ function loseGame(){
   if (imageNumber == 6) {
     imageNumber = 0;
     $(".notification").text('YOU LOSE!')
+    $(".notification").css("color","red")
   }
 }
 
@@ -125,22 +142,46 @@ function changeImage(){
   $(".hangman > img").attr("src", images[imageNumber])
 }
 
-//if "instructions" is hovered over, make panel appear
-$(".instructions").mouseenter(showPanel)
-$(".instructions").mouseleave(hidePanel)
+//if "instructions" is clicked, make panel appear
+$(".instructions").one("click",showInstructions)
+//if "categories" is hovered over, make panel appear
+$(".categories").one("click", showCategories)
 
-function hidePanel(){
+function hideInstructions(){
+  event.preventDefault()
   $("#instructions-panel").animate({
     height: "-=150px"
   })
-  $("#instructions-panel").html("")
+  $("#instructions-panel").empty()
+  $(".instructions").one("click", showInstructions)
 }
 
-function showPanel(){
+function showInstructions(){
+  event.preventDefault()
   $("#instructions-panel").animate({
     height: "+=150px"
   })
   $("#instructions-panel").html("<p>Guess a letter. If you guess incorrectly, your score will decrease and a piece of the skeleton will appear. If your score reaches zero, you lose!</p>")
+  $(".instructions").one("click", hideInstructions)
 }
+
+function hideCategories(){
+  event.preventDefault()
+  $("#categories-panel").animate({
+    height: "-=250px"
+  })
+  $("#categories-panel").empty()
+  $(".categories").one("click", showCategories)
+}
+
+function showCategories(){
+  event.preventDefault()
+  $("#categories-panel").animate({
+    height: "+=250px"
+  })
+  $("#categories-panel").html("<a>songs</a><a>food</a><a>colors</a><a>countries</a>")
+  $(".categories").one("click", hideCategories)
+}
+
 
 // });
